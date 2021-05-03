1dce4795525f72f3de0a6c643c8569d57c9dbde0
Clarence <clarencelindh@gmail.com>
2021-05-03 11:19:44 +0200

    Testrun backend

--- Backend/src/main/java/com/example/Holidaymaker/HolidayMakerApplication.java
+++ Backend/src/main/java/com/example/Holidaymaker/HolidayMakerApplication.java
@@ -8,6 +8,8 @@ public class HolidayMakerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolidayMakerApplication.class, args);
+
+		System.out.println("Started!");
 	}
 
 }

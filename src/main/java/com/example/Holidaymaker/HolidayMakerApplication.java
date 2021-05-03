35dfce33396f90121252e9f477205cddbf8a93d0
Stefan <Stefan_mijatovic97@hotmail.com>
2021-05-03 09:54:28 +0200

    first commit

---
+++ src/main/java/com/example/Holidaymaker/HolidayMakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Holidaymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Holidaymaker/HolidayMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Holidaymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

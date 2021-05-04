3790864cfc97f4fa0c01f8bb79f9570de6361979
Elias Rundqvist <rundqvistelias@gmail.com>
2021-05-04 11:40:35 +0200

    Userbackend

---
+++ Backend/src/main/java/com/example/Holidaymaker/entities/User.java
@@ -0,0 +1,57 @@
+package com.example.Holidaymaker.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

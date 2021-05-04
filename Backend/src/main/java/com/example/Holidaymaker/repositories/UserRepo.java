e48a5d973cbbde07c293b3cfc4b395261126195f
Stefan <Stefan_mijatovic97@hotmail.com>
2021-05-04 13:19:22 +0200

    merge get hotels to dev

--- Backend/src/main/java/com/example/Holidaymaker/repositories/UserRepo.java
+++ Backend/src/main/java/com/example/Holidaymaker/repositories/UserRepo.java
@@ -2,7 +2,9 @@ package com.example.Holidaymaker.repositories;
 
 import com.example.Holidaymaker.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }

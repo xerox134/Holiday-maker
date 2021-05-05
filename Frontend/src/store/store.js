08959d9a4bbb65b14639e7c3a0b313f7741688c9
Abbas <contactabbas07@gmail.com>
2021-05-05 11:59:22 +0200

    Favorite Frontend Created

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,12 +1,37 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
+
   state: {
+    favorites: [],
+    addedFavorites:[]
   },
+  
   mutations: {
+    setFavorites(state,payload){
+      state.favorites = payload;
+    }
   },
+  
   actions: {
+    async fetchAllFavorites(){
+      await axios.get("http://localhost:3000/rest/favorites/1")
+      .then(response => {
+      this.commit("setFavorites", response.data)
+      console.log(response.data)
+    })
   },
+  },
+
+  getters:{
+    getAllFavorites(state){
+      return state.favorites
+    }
+
+  },
+
+
   modules: {
   }
 })
--- Frontend/src/views/Favorites.vue
+++ Frontend/src/views/Favorites.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="favorites">
     <h1>All Favorites</h1>
+        <div id="Favorites"><Favorites/></div>
+
   </div>
 </template>
+
+<script>
+import Favorites from "../components/Favorites.vue"
+export default {
+  name: "favorites",
+  components: {
+    Favorites
+  },
+}
+</script>
+
+
+<style scoped>
+.favs{
+  
+}
+#Favorites {
+  background-color: rgba(60, 55, 65, .3);
+  color: blanchedalmond;
+  width: 100%;
+  
+  height: 100%;
+  box-shadow: 6px 6px 3px rgba(0, 0, 0, .6), inset 3px 3px 3px rgba(240, 200, 255, .05);
+  justify-content: center;
+  min-height: 15vh;
+
+}
+</style>

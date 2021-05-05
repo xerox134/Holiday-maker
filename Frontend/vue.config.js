ac5268fd002f4b189558acd16a7028a3060774c6
Stefan <Stefan_mijatovic97@hotmail.com>
2021-05-05 10:38:49 +0200

    Router/Store/Port fix

---
+++ Frontend/vue.config.js
@@ -0,0 +1,34 @@
+// vue.config.js
+module.exports = {
+    // https://cli.vuejs.org/config/#devserver-proxy
+    devServer: {
+        port: 3000,
+        proxy: {
+            '/rest': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+            '/api': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+            '/auth': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+            '/login': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+            '/logout': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+        },       
+    }
+}

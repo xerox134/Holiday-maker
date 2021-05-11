db5a83c81a16767acfe185fc9d09225cc48283d6
Stefan <Stefan_mijatovic97@hotmail.com>
2021-05-11 10:34:04 +0200

    comm1

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -8,9 +8,9 @@
  
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/favorites">Favorites</router-link> |
+    <router-link to="/favorites">Favorites</router-link> 
    
-    <h1 id="title">Holidaymaker</h1>
+   
   </div>
 
 <div id="login">

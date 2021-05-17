6f08c250622d8d06356de0fc70910cce75ab3b0b
Stefan <Stefan_mijatovic97@hotmail.com>
2021-05-17 15:15:40 +0200

    1.5 commit 1

--- Frontend/src/components/SearchBar.vue
+++ Frontend/src/components/SearchBar.vue
@@ -1,13 +1,48 @@
 <template>
-  <form id="search-container">
-      <input type="search" placeholder="Sök ...">
+ 
+ <form action="rest/hotel/search/">
+<div id="SearchContainer">
 
-  </form>
+   <div class="container">
+   
+      <input v-model="search_phrase" type="text" placeholder="Search hotels..." name="ename" required>
+   
+      <button type="submit">Search</button>
+     
+      </div>
+
+
+</div>
+</form>
+
+
 </template>
 
 <script>
 export default {
+  name:"SearchBar",
 
+ data (){
+
+  
+    return{
+      
+        search_phrase: '',
+     
+      
+       
+    }
+
+},
+
+  methods: {
+     check () {
+      // tell backend to forget us
+      console.log(this.search_phrase)
+      
+
+}
+  }
 }
 </script>
 

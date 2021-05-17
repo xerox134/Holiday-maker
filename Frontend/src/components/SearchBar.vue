f5a8b3e9dfde311df57b02e7bc91b72be606f390
Elias Rundqvist <rundqvistelias@gmail.com>
2021-05-17 11:27:00 +0200

    searchcomponent

---
+++ Frontend/src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <form id="search-container">
+      <input type="search" placeholder="Sök ...">
+
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

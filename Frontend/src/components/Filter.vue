7b1b82aca97132a76997bfb63e6aa1d556e58a37
Anton Baric <anton.baric99@gmail.com>
2021-05-24 15:01:12 +0200

    Ändrade kommentarer och loggar. Tog bort kod som inte används

--- Frontend/src/components/Filter.vue
+++ Frontend/src/components/Filter.vue
@@ -71,43 +71,47 @@ export default {
       console.log(list1)
       
     },
-          async togglePool(includesPool) {
-            console.log(includesPool)
-            this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.pool == includesPool
-            
-         })
-        },
-          async toggleEntertainment(includesEntertainment) {
-            console.log(includesEntertainment)
-            this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.entertainment == includesEntertainment
-            
-         })
-        },
-        async toggleChildrensClub(includesChildrensClub) {
-          this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.childrens_club == includesChildrensClub
-          })
-        },
-        async toggleRestaurant(includesRestaurant) {
-          this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.restaurant == includesRestaurant
-          })
-        },
-        async toggleWifi(includesWifi) {
-          this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.wifi == includesWifi
-          })
-        },
-        async toggleBar(includesBar) {
-          this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
-            return hotel.bar == includesBar
-          })
-        },
-        async clear() {
-            return this.$store.dispatch("fetchHotels")
-        }
+    async togglePool(includesPool) {
+      console.log("Pool is " + includesPool)
+      // metoden filtrerar hotels-arrayen. Den plockar ut alla hotell som inte har pool
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+      return hotel.pool == includesPool
+      })
+    },
+    async toggleEntertainment(includesEntertainment) {
+      console.log("Entertainment is " + includesEntertainment)
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+        return hotel.entertainment == includesEntertainment   
+      })
+    },
+    async toggleChildrensClub(includesChildrensClub) {
+      console.log("Childrens Club is " + includesChildrensClub)
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+        return hotel.childrens_club == includesChildrensClub
+      })
+    },
+    async toggleRestaurant(includesRestaurant) {
+      console.log("Restaurant is " + includesRestaurant)
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+        return hotel.restaurant == includesRestaurant
+      })
+    },
+    async toggleWifi(includesWifi) {
+      console.log("Wifi is " + includesWifi)
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+        return hotel.wifi == includesWifi
+      })
+    },
+    async toggleBar(includesBar) {
+      console.log("Bar is " + includesBar)
+      this.$store.state.hotels = this.$store.state.hotels.filter(hotel => {
+        return hotel.bar == includesBar
+      })
+    },
+    async clear() {
+      console.log("Filters cleared")
+        return this.$store.dispatch("fetchHotels")
+    }
         
   }
 

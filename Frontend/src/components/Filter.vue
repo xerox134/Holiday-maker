a8ed447d36263c0c95c851263a81321095686a6e
Anton Baric <anton.baric99@gmail.com>
2021-05-24 15:01:34 +0200

    Denna också

--- Frontend/src/components/Filter.vue
+++ Frontend/src/components/Filter.vue
@@ -11,66 +11,21 @@
     <button @click="toggleBar(true)">Bar</button>
     <button @click="clear()">Clear filters</button>
 
-
-    <ol id="FilteredHotelList">
-      <li v-for="(hotel, index) in getFilteredHotels"  :key="index">
-        <Card :card="hotel"  :type="'hotel'"/>
-      </li>
-    </ol>
-
   </div>
 </template>
 
 
 <script>
-import Card from "./Card"
 export default {
   name: "Filter",
 
   components: {
-    Card
   },
 
   computed: {
-    getFilteredHotels(){
-      return this.$store.getters.getFilteredHotels
-    },
-    getHotelsWithPool(){
-      return this.$store.getters.getHotelsWithPool
-    },
-    getHotelsWithEntertainment(){
-      return this.$store.getters.getHotelsWithEntertainment
-    },
   },
 
   methods:{
-    searchForPool(){
-      this.$store.dispatch("fetchHotelByPool");
-    },
-    searchForEntertainment(){
-      this.$store.dispatch("fetchHotelByEntertainment");
-    },
-    searchForRestaurant(){
-      this.$store.dispatch("fetchHotelByRestaurant");
-    },
-    searchForWifi(){
-      this.$store.dispatch("fetchHotelByWifi");
-    },
-    searchForBar(){
-      this.$store.dispatch("fetchHotelByBar");
-    },
-    combineFilters(){
-      let list1 = this.$store.getFilteredHotels
-      let list2 = this.$store.getHotelsWithPool
-      Array.prototype.push.apply(list1, 
-      list2)
-      console.log(list1)
-    },
-    test(){
-      let list1 = this.$store.getHotelsWithPool
-      console.log(list1)
-      
-    },
     async togglePool(includesPool) {
       console.log("Pool is " + includesPool)
       // metoden filtrerar hotels-arrayen. Den plockar ut alla hotell som inte har pool

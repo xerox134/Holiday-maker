08f035d8fdb3bf9f8e4b19316837940c5820c5ec
Elias Rundqvist <rundqvistelias@gmail.com>
2021-05-28 10:46:33 +0200

    början på antal personer i varukorg

--- Frontend/src/components/Ongoingbooking.vue
+++ Frontend/src/components/Ongoingbooking.vue
@@ -5,6 +5,17 @@
 <p>Total price : {{ updatePrice }} kr</p>
 
 </div>
+ <div class="dropdown">
+<button class="dropbtn">Antal personer</button>
+<div class="dropdown-content">
+  <a href="#">1</a>
+  <a href="#">2</a>
+  <a href="#">3</a>
+
+
+
+</div>
+</div>
 
 
   <div id="OngoingbookingList">
@@ -38,4 +49,26 @@ export default {
 
 <style>
 
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown:hover .dropdown-content {display: block;}
 </style>

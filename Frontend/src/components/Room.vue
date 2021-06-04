105bd43256fb30d4350559573ed4f5cfcdd7aa55
Stefan <Stefan_mijatovic97@hotmail.com>
2021-06-04 22:17:13 +0200

    Filter booked rooms done

--- Frontend/src/components/Room.vue
+++ Frontend/src/components/Room.vue
@@ -1,21 +1,22 @@
 <template>
-<button @click="$router.push('ongoingbooking')">Continue to booking</button>
-    <div>
-        <ol id="HotelList">
-            <li v-for="(hotel, index) in getHotelById"  :key="index" >   <!--false=boolean till episodeToggle-->
-                <Card :card="hotel"  :type="'hotel'"/>  
-            </li>
-        </ol>
-    </div>
-    <h2>Available rooms</h2>
-    <div>
-        <ol  id="RoomsList">
-            <li v-for="(room, index) in getRoomByHotelId"  :key="index" >   <!--false=boolean till episodeToggle-->
-                <Card :card="room"  :type="'room'"/>  
-            </li>
-        </ol>
-  
-    </div>
+  <button @click="$router.push('ongoingbooking')">Continue to booking</button>
+  <div>
+    <ol id="HotelList">
+      <li v-for="(hotel, index) in getHotelById" :key="index">
+        <!--false=boolean till episodeToggle-->
+        <Card :card="hotel" :type="'hotel'" />
+      </li>
+    </ol>
+  </div>
+  <h2>Available rooms</h2>
+  <div>
+    <ol id="RoomsList">
+      <li v-for="(room, index) in getRoomByHotelId" :key="index">
+        <!--false=boolean till episodeToggle-->
+        <Card :card="room" :type="'room'" />
+      </li>
+    </ol>
+  </div>
 </template>
 
 
@@ -30,7 +31,7 @@ export default {
       filterdArray: [],
       resultArr: [],
       emptyArray: [],
-      pointer:0
+      pointer: 0,
     };
   },
 
@@ -38,122 +39,118 @@ export default {
     Card,
   },
 
-    
-    computed: {
-        getRoomByHotelId(){
-            return this.$store.getters.getRoomByHotelId
-        }, 
-        getHotelById(){
-            return this.$store.getters.getHotelById
-        },
-        filteredRooms() {
-           return this.$store.state.rooms.filter(room => {
-            return room.beds >= this.$store.getters.getAllPeople
-
-        });
-        },
+  computed: {
+    getRoomByHotelId() {
+      return this.$store.getters.getRoomByHotelId;
     },
-  
+    getHotelById() {
+      return this.$store.getters.getHotelById;
+    },
+    filteredRooms() {
+      return this.$store.state.rooms.filter((room) => {
+        return room.beds >= this.$store.getters.getAllPeople;
+      });
+    },
+  },
 
-    methods:{
-        toggle () {
-        this.active = !this.active
-        
+  methods: {
+    toggle() {
+      this.active = !this.active;
     },
 
     filterRoomsDate() {
+      // JÄMFÖR OM DET FINNS BOOKINGS SOM ÄR BOKADE UNDER DATUM
+      console.log("FilterRoomsDate börjar");
+      this.$store.state.allBookings.forEach((element) => {
+        console.log("datum före:", this.$store.state.fromDate);
+        console.log("datum efter:", this.$store.state.toDate);
+        console.log("booking före:", element.fromDate);
+        console.log("booking efter:", element.toDate);
+        if (
+          this.$store.state.fromDate <= element.fromDate &&
+          this.$store.state.toDate >= element.fromDate &&
+          this.$store.state.toDate <= element.toDate
+        ) {
+          console.log("FÖRE INNE");
+          this.pointer = 1;
+        } else if (
+          this.$store.state.fromDate >= element.fromDate &&
+          this.$store.state.fromDate <= element.toDate &&
+          this.$store.state.toDate >= element.toDate
+        ) {
+          console.log("INNE EFTER");
+          this.pointer = 1;
+        } else if (
+          this.$store.state.fromDate <= element.fromDate &&
+          this.$store.state.toDate >= element.toDate
+        ) {
+          console.log("FÖRE EFTER");
+          this.pointer = 1;
+        } else if (
+          this.$store.state.fromDate >= element.fromDate &&
+          this.$store.state.toDate <= element.toDate
+        ) {
+          console.log("INNE INNE");
+          this.pointer = 1;
+        } else console.log("ingen funka!");
+      });
 
+      //IFALL DET FINNS SÅ GÅR VI IN I METODERNA ANNARS RETURERAR VI ENDAST ROOMS DIREKT
 
-        // JÄMFÖR OM DET FINNS BOOKINGS SOM ÄR BOKADE UNDER DATUM
-        console.log("inne nu");
-        this.$store.state.allBookings.forEach(element => {
-            console.log("datum före:",this.$store.state.fromDate )
-            console.log("datum efter:",this.$store.state.toDate )
-            console.log("booking före:",element.fromDate )
-            console.log("booking efter:",element.toDate )
-            if(this.$store.state.fromDate<=element.fromDate && this.$store.state.toDate>=element.fromDate && this.$store.state.toDate<=element.toDate ){
-                console.log("FÖRE INNE", )
-                this.pointer=1
-          
-          } else if(this.$store.state.fromDate>= element.fromDate && this.$store.state.fromDate <= element.toDate && this.$store.state.toDate>=element.toDate){
-                console.log("INNE EFTER", )
-                this.pointer=1
-         
-          }else if(this.$store.state.fromDate<=element.fromDate && this.$store.state.toDate>=element.toDate){
-              console.log("FÖRE EFTER")
-                this.pointer=1
+      if (this.pointer == 0) {
+        console.log("endast rooms");
+        return this.$store.state.rooms;
+      } else {
+        // METODERNA BÖRJAR!
+        console.log("everything");
+        this.filterBookableRooms();
+        this.filterRoomsWithBookedRooms();
+        this.fliterDuplicateRooms();
+        this.makeRoomsArrayWork();
 
-          }else if(this.$store.state.fromDate>=element.fromDate && this.$store.state.toDate<=element.toDate){
-                console.log("INNE INNE")
-                 this.pointer=1
-
-         }  else console.log("ingen funka!")
-  });
-
-        //IFALL DET FINNS SÅ GÅR VI IN I METODERNA ANNARS RETURERAR VI ENDAST ROOMS DIREKT
-
-if(this.pointer==0){
-    console.log("endast rooms")
-    return this.$store.state.rooms
-        } else{
-
-
-            // METODERNA BÖRJAR!
-        console.log("everything")
-         this.filterBookableRooms();
-        //  this.filterBookableRooms2();
-         this.filterRoomsWithBookedRooms();
-         this.fliterDuplicateRooms();
-         this.makeRoomsArrayWork();
-
-      console.log("detta är resultarr", this.resultArr);
-      this.pointer=0
-        }
-},
-
-
-    filterBookableRooms() {
-  console.log("1");
-        this.$store.state.allBookings = this.$store.state.allBookings.filter(
-        (element) => {
-             if(this.$store.state.fromDate<=element.fromDate && this.$store.state.toDate>=element.fromDate && this.$store.state.toDate<=element.toDate ){
-                console.log("FÖRE INNE", )
-                return element
-          
-          } else if(this.$store.state.fromDate>= element.fromDate && this.$store.state.fromDate <= element.toDate && this.$store.state.toDate>=element.toDate){
-                console.log("INNE EFTER", )
-              return element
-         
-          }else if(this.$store.state.fromDate<=element.fromDate && this.$store.state.toDate>=element.toDate){
-              console.log("FÖRE EFTER")
-               return element
-
-          }else if(this.$store.state.fromDate>=element.fromDate && this.$store.state.toDate<=element.toDate){
-                console.log("INNE INNE")
-                return element
-
-         }
-          
-          }
-      )
-      
+        console.log("detta är resultarr", this.resultArr);
+        this.pointer = 0;
+      }
     },
 
-
-
-    filterBookableRooms2() {
-      console.log("1sta filter " ,this.$store.state.allBookings)
-      console.log("2");
+    filterBookableRooms() {
+      console.log("1");
       this.$store.state.allBookings = this.$store.state.allBookings.filter(
-        (bookedRooms) => {
-            
-          return bookedRooms.toDate <= this.$store.state.toDate;
+        (element) => {
+          if (
+            this.$store.state.fromDate <= element.fromDate &&
+            this.$store.state.toDate >= element.fromDate &&
+            this.$store.state.toDate <= element.toDate
+          ) {
+            console.log("FÖRE INNE");
+            return element;
+          } else if (
+            this.$store.state.fromDate >= element.fromDate &&
+            this.$store.state.fromDate <= element.toDate &&
+            this.$store.state.toDate >= element.toDate
+          ) {
+            console.log("INNE EFTER");
+            return element;
+          } else if (
+            this.$store.state.fromDate <= element.fromDate &&
+            this.$store.state.toDate >= element.toDate
+          ) {
+            console.log("FÖRE EFTER");
+            return element;
+          } else if (
+            this.$store.state.fromDate >= element.fromDate &&
+            this.$store.state.toDate <= element.toDate
+          ) {
+            console.log("INNE INNE");
+            return element;
+          }
         }
       );
     },
+
     filterRoomsWithBookedRooms() {
-        console.log("2a filter" ,this.$store.state.allBookings)
-      console.log("3");
+      console.log("2a filter", this.$store.state.allBookings);
+      console.log("2");
       this.$store.state.allBookings.forEach((element) => {
         this.id.push(element.room.id);
       }),
@@ -170,34 +167,33 @@ if(this.pointer==0){
               return true;
             } else {
               console.log("Inne på funkar ej");
-              console.log("funkar ej:", element2.id,element3)
-              var k = this.id.length
-             var i;
-                for (i = 0; i < k; i++) {
-                 this.filterdArray.pop(element2)
-                 console.log(k)
-                }
-             
-              
+              console.log("funkar ej:", element2.id, element3);
+              var k = this.id.length;
+              var i;
+              for (i = 0; i < k; i++) {
+                this.filterdArray.pop(element2);
+                console.log(k);
+              }
+
               return false;
             }
           });
         });
     },
     fliterDuplicateRooms() {
-      console.log("4");
+      console.log("3");
       this.resultArr = this.filterdArray.filter((data, index) => {
         return this.filterdArray.indexOf(data) === index;
       });
     },
     makeRoomsArrayWork() {
-      console.log("5");
+      console.log("4");
       this.$store.state.rooms = this.resultArr;
       console.log("sista funktionen");
-      this.$store.state.allBookings=this.$store.state.allBookings2
-      this.filterdArray=this.emptyArray
+      this.$store.state.allBookings = this.$store.state.allBookings2;
+      this.filterdArray = this.emptyArray;
     },
-    },
+  },
 
   mounted() {
     console.log("we fetching");
@@ -205,7 +201,7 @@ if(this.pointer==0){
     this.$store.dispatch("fetchRoomsByHotelId");
     this.$store.dispatch("fetchBookings");
 
-setTimeout(() => {
+    setTimeout(() => {
       this.filterRoomsDate();
     }, 500);
 
